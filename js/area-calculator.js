fb706ffd55d66f1b7eb5e83abc2b15c0b3378a41
mdmomenulislam1 <mdmomenulislam1@gmail.com>
2023-08-18 15:49:35 +0600

    Last commit

--- js/area-calculator.js
+++ js/area-calculator.js
@@ -45,47 +45,106 @@ function setElementInnerText(elementId, Area) {
     element.innerText = Area;
 }
 
+function addCalculationEntry(areaType, area){
+    console.log(areaType+ ' '+ area);
+    const calculationEntry = document.getElementById('calculation-entry');
+
+    const count = calculationEntry.childElementCount;
+
+    const p = document.createElement('p');
+    p.classList.add('my-3','font-bold')
+    p.innerHTML = `${count + 1}. ${  areaType }<span>  </span> ${area} cm<sup>2</sup><span> </span><button id="meter-square" class="rounded-lg bg-green-500 text-white p-2">Convert m<sup>2</sup></button>`;
+    calculationEntry.appendChild(p);
+}
+
 function calculateTriangleArea(){
     const base = getInputValue('triangle-base');
     const height = getInputValue('triangle-height');
+
+    if (isNaN(base)|| isNaN(height) || base <= 0 || height <= 0) {
+        alert('Insert a positive number')
+        return;
+    }
+
     const area = 0.5*base*height;
-    setElementInnerText('triangle-area', area);
+    const areaThreeDecimal = area.toFixed(3);
+    setElementInnerText('triangle-area', areaThreeDecimal);
+    addCalculationEntry('Triangle', areaThreeDecimal);
 }
 
 function calculateRectangleArea(){
     const width = getInputValue('rectangle-width');
     const length = getInputValue('rectangle-length');
+
+    if (isNaN(width)|| isNaN(length) || width <= 0 || length <= 0) {
+        alert('Insert a positive number')
+        return;
+    }
+
     const area = width * length;
-    setElementInnerText('rectangle-area', area)
+    const areaThreeDecimal = area.toFixed(3);
+    setElementInnerText('rectangle-area', areaThreeDecimal);
+    addCalculationEntry('Rectangle', areaThreeDecimal);
 }
 
 function calculateParallelogramArea(){
     const base = getInputValue('parallelogram-base');
     const height = getInputValue('parallelogram-height');
+
+    if (isNaN(base)|| isNaN(height) || base <= 0 || height <= 0) {
+        alert('Insert a positive number')
+        return;
+    }
+
     const area = base * height;
-    setElementInnerText('parallelogram-area', area)
+    const areaThreeDecimal = area.toFixed(3);
+    setElementInnerText('parallelogram-area', areaThreeDecimal);
+
+    addCalculationEntry('Parallelogram', areaThreeDecimal);
 }
 
 function calculateRhombusArea(){
     const width = getInputValue('rhombus-width');
     const length = getInputValue('rhombus-length');
+
+    if (isNaN(width)|| isNaN(length) || width <= 0 || length <= 0) {
+        alert('Insert a positive number')
+        return;
+    }
+
     const area = 0.5*width*length;
-    
-    setElementInnerText('rhombus-area', area);
+    const areaThreeDecimal = area.toFixed(3);
+    setElementInnerText('rhombus-area', areaThreeDecimal);
+    addCalculationEntry('Rhombus', areaThreeDecimal);
 }
 
 function calculatePentagonArea(){
     const width = getInputValue('pentagon-width');
     const length = getInputValue('pentagon-length');
-    const area =0.5 *width*length;
 
-    setElementInnerText('pentagon-area', area);
+    if (isNaN(width)|| isNaN(length) || width <= 0 || length <= 0) {
+        alert('Insert a positive number')
+        return;
+    }
+
+    const area =0.5 *width*length;
+    const areaThreeDecimal = area.toFixed(3);
+    setElementInnerText('pentagon-area', areaThreeDecimal);
+    addCalculationEntry('Pentagon', areaThreeDecimal);
 }
 
 function calculateEllipseArea(){
     const max_radius = getInputValue('ellipse-max-radius');
     const min_radius = getInputValue('ellipse-min-radius');
+
+    if (isNaN(max_radius)|| isNaN(min_radius) || max_radius <= 0 || min_radius <= 0) {
+        alert('Insert a positive number')
+        return;
+    }
+
     const area = Math.PI*max_radius*min_radius;
-    const areaTwoDecimal = area.toFixed(2);
-    setElementInnerText('ellipse-area',areaTwoDecimal);
+    const areaThreeDecimal = area.toFixed(3);
+    setElementInnerText('ellipse-area',areaThreeDecimal);
+    addCalculationEntry('Ellipse', areaThreeDecimal);
 }
+
